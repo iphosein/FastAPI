1cb8f69d7fd4d15e1e59fecacf9a4ec41048a24f
Hosein <230954975+iphosein@users.noreply.github.com>
2025-11-29 17:58:34 +0330

    Update database.py

--- app/database.py
+++ app/database.py
@@ -11,8 +11,10 @@ from .config import settings
 
 # SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
+SQLALCHEMY_DATABASE_URL = settings.database_url
 
-engine = create_engine(settings.database_url)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(settings.database_url)
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
